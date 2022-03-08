8e0fcd83b08be2d0dd020491a099e9fc3d2a2bf4
yang <1126923037@qq.com>
2022-03-08 18:33:17 +0800

    Optimize network type changes

--- taucoin-android/src/main/java/io/taucoin/torrent/publishing/receiver/ConnectionReceiver.java
+++ taucoin-android/src/main/java/io/taucoin/torrent/publishing/receiver/ConnectionReceiver.java
@@ -5,15 +5,20 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.net.ConnectivityManager;
+import android.net.LinkAddress;
+import android.net.LinkProperties;
+import android.net.Network;
 import android.net.NetworkInfo;
-import android.net.wifi.WifiInfo;
-import android.net.wifi.WifiManager;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import io.taucoin.torrent.publishing.core.storage.sp.SettingsRepository;
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.util.List;
+
 import io.taucoin.torrent.publishing.core.storage.RepositoryHelper;
+import io.taucoin.torrent.publishing.core.storage.sp.SettingsRepository;
 import io.taucoin.torrent.publishing.core.utils.NetworkSetting;
 
 /*
@@ -32,22 +37,53 @@ public class ConnectionReceiver extends BroadcastReceiver {
         if (action.equals(ConnectivityManager.CONNECTIVITY_ACTION)) {
             ConnectivityManager connectivityManager = (ConnectivityManager)
                     appContext.getSystemService(Context.CONNECTIVITY_SERVICE);
-            NetworkInfo info = connectivityManager.getActiveNetworkInfo();
-            logger.debug("NetworkType::{}, TypeName::{}, isActiveNetworkMetered::{}",
-                    info != null ? info.getType() : null,
-                    info != null ? info.getTypeName() : null,
-                    connectivityManager.isActiveNetworkMetered());
-            NetworkSetting.setMeteredNetwork(info != null && connectivityManager.isActiveNetworkMetered());
-            if (info != null && info.isConnected()) {
-                WifiManager wifiManager = (WifiManager) appContext.getSystemService(Context.WIFI_SERVICE);
-                WifiInfo wifiInfo = wifiManager.getConnectionInfo();
-                logger.debug("TYPE::{}, SSID::{}, BSSID::{}, LinkSpeed::{}", info.getType(), wifiInfo.getSSID(),
-                        wifiInfo.getBSSID(), wifiInfo.getLinkSpeed());
+            NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+            boolean isMetered = activeNetworkInfo != null && connectivityManager.isActiveNetworkMetered();
+            NetworkSetting.setMeteredNetwork(isMetered);
+            if (activeNetworkInfo != null && activeNetworkInfo.isConnected()) {
+                NetworkInfo newNetworkInfo = activeNetworkInfo;
+                Network[] allNetworks = connectivityManager.getAllNetworks();
+                if (allNetworks != null) {
+                    for (Network network : allNetworks) {
+                        NetworkInfo networkInfo = connectivityManager.getNetworkInfo(network);
+                        if (networkInfo != null) {
+                            parseNetworkAddress(networkInfo.getType(), connectivityManager.getLinkProperties(network));
+                            if (networkInfo.isConnected()) {
+                                if ( networkInfo.getType() > newNetworkInfo.getType()) {
+                                    newNetworkInfo = networkInfo;
+                                }
+                            }
+                        }
+                    }
+                }
+                logger.debug("network type::{}, activeNetworkInfo::{}",
+                        newNetworkInfo.getType(), newNetworkInfo);
                 settingsRepo.internetState(true);
-                settingsRepo.setInternetType(info.getType());
+                settingsRepo.setInternetType(newNetworkInfo.getType());
             } else {
+                logger.debug("network type::-1, activeNetworkInfo::{}",
+                        null == activeNetworkInfo ? "null" : activeNetworkInfo);
                 settingsRepo.internetState(false);
                 settingsRepo.setInternetType(-1);
+            }
+        }
+    }
+
+    private void parseNetworkAddress(int type, LinkProperties linkProperties) {
+        if (linkProperties != null) {
+            List<LinkAddress> linkAddresses = linkProperties.getLinkAddresses();
+            if (linkAddresses != null) {
+                for (LinkAddress linkAddress : linkAddresses) {
+                    InetAddress address = linkAddress.getAddress();
+                    if (address != null) {
+                        if (address instanceof Inet4Address && !address.isLoopbackAddress() &&
+                                !address.isLinkLocalAddress()) {
+                            String ipv4 = address.getHostAddress();
+                            logger.debug("parseNetworkAddress type::{} IPv4::{}", type, ipv4);
+                            break;
+                        }
+                    }
+                }
             }
         }
     }
