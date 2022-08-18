d5850c05630f8d4047efa416c5f77a4296bf7739
yang <1126923037@qq.com>
2022-08-18 22:18:41 +0800

    Remove org.jetbrains.annotations.NotNull

--- taucoin-android/src/main/java/io/taucoin/torrent/publishing/ui/BaseFragment.java
+++ taucoin-android/src/main/java/io/taucoin/torrent/publishing/ui/BaseFragment.java
@@ -5,8 +5,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-import org.jetbrains.annotations.NotNull;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
